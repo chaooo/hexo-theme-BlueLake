83c5636e840664d31ae211e9641787aa2a136f8a
chao <chaoles@hotmail.com>
2020-12-28 15:37:45 +0800

    PC布局init

--- source/js/script.js
+++ source/js/script.js
@@ -32,59 +32,59 @@
   });
 
   // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
+  // $('body').on('click', function(){
+  //   $('.article-share-box.on').removeClass('on');
+  // }).on('click', '.article-share-link', function(e){
+  //   e.stopPropagation();
 
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      title = $this.attr('data-title'),
-      offset = $this.offset();
+  //   var $this = $(this),
+  //     url = $this.attr('data-url'),
+  //     encodedUrl = encodeURIComponent(url),
+  //     id = 'article-share-box-' + $this.attr('data-id'),
+  //     title = $this.attr('data-title'),
+  //     offset = $this.offset();
 
-    if ($('#' + id).length){
-      var box = $('#' + id);
+  //   if ($('#' + id).length){
+  //     var box = $('#' + id);
 
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="LinkedIn"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
+  //     if (box.hasClass('on')){
+  //       box.removeClass('on');
+  //       return;
+  //     }
+  //   } else {
+  //     var html = [
+  //       '<div id="' + id + '" class="article-share-box">',
+  //         '<input class="article-share-input" value="' + url + '">',
+  //         '<div class="article-share-links">',
+  //           '<a href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+  //           '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+  //           '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+  //           '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="LinkedIn"></a>',
+  //         '</div>',
+  //       '</div>'
+  //     ].join('');
 
-      var box = $(html);
+  //     var box = $(html);
 
-      $('body').append(box);
-    }
+  //     $('body').append(box);
+  //   }
 
-    $('.article-share-box.on').hide();
+  //   $('.article-share-box.on').hide();
 
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
+  //   box.css({
+  //     top: offset.top + 25,
+  //     left: offset.left
+  //   }).addClass('on');
+  // }).on('click', '.article-share-box', function(e){
+  //   e.stopPropagation();
+  // }).on('click', '.article-share-box-input', function(){
+  //   $(this).select();
+  // }).on('click', '.article-share-box-link', function(e){
+  //   e.preventDefault();
+  //   e.stopPropagation();
 
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
+  //   window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
+  // });
 
   // Caption
   $('.article-entry').each(function(i){

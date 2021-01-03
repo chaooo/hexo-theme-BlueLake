fbd4a83180d13026079593820a52ba555aa085c6
chaoo <chaoles@foxmail.com>
2021-01-03 21:42:48 +0800

    add dark theme

---
+++ source/js/totop.js
@@ -0,0 +1,43 @@
+function hasClass(obj, cls) {
+    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+function addClass(obj, cls) {
+    if (!hasClass(obj, cls)) obj.className += " " + cls;
+}
+function removeClass(obj, cls) {
+    if (hasClass(obj, cls)) {
+        var reg = new RegExp('(\\s+|^)' + cls + '(\\s|$)');
+        obj.className = obj.className.replace(reg, ' ');
+    }
+}
+window.onscroll = function() {
+  var totop = document.getElementById('totop');
+  var scroll = document.documentElement.scrollTop || document.body.scrollTop || window.scrollY;
+  if (scroll >= 300) {
+    addClass(totop,"show");
+    //totop.classList.add("show");
+  } else {
+    removeClass(totop,"show");
+    removeClass(totop,"launch");
+    //totop.classList.remove("show", "launch");
+  }
+};
+function gotoTop(aSpeed, time) {
+  aSpeed = aSpeed || 0.1;
+  time = time || 10;
+  var totop = document.getElementById('totop');
+  var scroll = document.documentElement.scrollTop || document.body.scrollTop || window.scrollY || 0;
+  var speeding = 1 + aSpeed;
+  window.scrollTo(0, Math.floor(scroll / speeding));
+  if (scroll > 0) {
+    var run = "gotoTop(" + aSpeed + ", " + time + ")";
+    window.setTimeout(run, time);
+  }
+}
+totop.onclick = function() {
+  var totop = document.getElementById('totop');
+  gotoTop(0.1, 20);
+  addClass(totop,"launch");
+  //totop.classList.add('launch');
+  return false;
+};
